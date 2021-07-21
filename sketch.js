7a8fd7a2c1c7465a1b539077acee9d8a62238609
Freena13 <86773045+Freena13@users.noreply.github.com>
2021-07-21 18:55:25 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var s1, s2;
+
+function setup() {
+  createCanvas(400,400);
+
+  s1 = new Student("Freena",12,2);
+  s2 = new Student("John",11,8);
+
+  s1.Student2();
+  s2.Student2();
+  
+}
+
+function draw() 
+{
+  background(30);
+ 
+  
+}
---
+++ student.js
@@ -0,0 +1,18 @@
+
+class Student
+{
+   constructor(name,age,number)
+   {
+    this.name = name;
+    this.age = age;
+    this.number = number;
+   }
+   Student2()
+   {
+       console.log(this.name);
+       console.log(this.age);
+       console.log(this.number);
+   }
+}
+
+
